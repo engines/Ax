f523ab2e8903c0524552873352078e796177e342
Lachlan Douglas <lachlan@engines.org>
2021-08-04 18:27:33 +1000

    v0.0.18

--- packages/src/ax-appkit-codemirror/extension/codemirror/form/control.js
+++ packages/src/ax-appkit-codemirror/extension/codemirror/form/control.js
@@ -51,7 +51,8 @@ ax.extension.codemirror.form.control = function (r, options = {}) {
       },
 
       $focus: (el) => () => {
-        el.$('textarea').$codemirror.focus();
+        let codemirror = el.$('textarea').$codemirror;
+        if (codemirror) codemirror.focus();
       },
 
       $enabled: !options.disabled,
